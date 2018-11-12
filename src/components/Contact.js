46b36cf0a07d5a83fa6dd2dfc08b1e87fa88c405
saliloak <salil.oak@hotmail.com>
2018-11-12 20:57:27 +0530

    Learned how to add State and Events

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -2,11 +2,27 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
 class Contact extends Component {
+  state = {};
+  // onShowClick = () => {
+  //   console.log('Hello');
+  // };
+  onShowClick = (name, e) => {
+    console.log(name);
+  };
   render() {
     const { name, email, phone } = this.props.contact;
     return (
       <div className="card card-body mb-3">
-        <h4>{name}</h4>
+        {/* <h4>
+          {name} <i onClick={this.onShowClick} className="fas fa-sort-down" />
+        </h4> */}
+        <h4>
+          {name}{' '}
+          <i
+            onClick={this.onShowClick.bind(this, name)}
+            className="fas fa-sort-down"
+          />
+        </h4>
         <ul className="list-group">
           <li className="list-group-item">Email: {email}</li>
           <li className="list-group-item">Phone Number: {phone}</li>
