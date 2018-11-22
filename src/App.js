25800816333d23107bcaa66a387fbf6b190b7b7b
saliloak <salil.oak@hotmail.com>
2018-11-22 19:20:17 +0530

    Learnt about react router

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Header from './components/layout/Header';
 import About from './components/pages/About';
 import AddContact from './components/contacts/AddContact';
 import { Provider } from './context';
-
+import NotFound from './components/pages/NotFound';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
@@ -21,6 +21,7 @@ class App extends Component {
                 <Route exact path="/" component={Contacts} />
                 <Route exact path="/contact/add" component={AddContact} />
                 <Route exact path="/about" component={About} />
+                <Route component={NotFound} />
               </Switch>
             </div>
           </div>
