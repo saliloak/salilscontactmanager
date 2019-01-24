7341fbeb8ac1f2746de10674fb79a589a0072c56
Salil <salil.oak@hotmail.com>
2019-01-24 11:42:33 +0530

    Trial Commit

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
-import React, { Component } from 'react';
-import { HashRouter as Router, Route, Switch } from 'react-router-dom';
-import Contacts from './components/contacts/Contacts';
-import Header from './components/layout/Header';
-import About from './components/pages/About';
-import AddContact from './components/contacts/AddContact';
-import EditContact from './components/contacts/EditContact';
-import { Provider } from './context';
-import NotFound from './components/pages/NotFound';
-import Test from './components/test/Test';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './App.css';
+// Test Comment
+import React, { Component } from "react";
+import { HashRouter as Router, Route, Switch } from "react-router-dom";
+import Contacts from "./components/contacts/Contacts";
+import Header from "./components/layout/Header";
+import About from "./components/pages/About";
+import AddContact from "./components/contacts/AddContact";
+import EditContact from "./components/contacts/EditContact";
+import { Provider } from "./context";
+import NotFound from "./components/pages/NotFound";
+import Test from "./components/test/Test";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
 
 class App extends Component {
   render() {
